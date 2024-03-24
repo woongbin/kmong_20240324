ab4d22402e6552b7b305ad7949c707ef08a35dd4
sirwoongke <fzwoongbin@naver.com>
2024-03-24 12:09:36 +0900

    add column

---
+++ database/migrations/2024_03_24_030745_add_book_table_column_page.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('book', function (Blueprint $table) {
+            $table->integer('page')->after('price');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('book', function (Blueprint $table) {
+            $table->dropColumn('page');
+        });
+    }
+};
